d351a990a65eeb93f52344dc286af27f196b4374
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-07-13 19:36:19 -0400

    exercise 11 completed

---
+++ exercise11and12.py
@@ -0,0 +1,17 @@
+#EXERCISE 11
+print("===START OF EXERCISE 11===")
+
+one_to_hundred = range(1, 101)
+
+for num in one_to_hundred:
+    if num % 3 == 0 and num % 5 == 0:
+        print ('BitMaker')
+    elif num % 3 == 0:
+        print ('Bit')
+    elif num % 5 == 0:
+        print ('Maker')
+    else: 
+        print (num)
+
+#EXERCISE 12
+print("===START OF EXERCISE 12===")
